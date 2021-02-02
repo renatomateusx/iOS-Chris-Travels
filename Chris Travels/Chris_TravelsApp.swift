4ca653c68ae4f84170c801ba800b0a476fcbc35c
Renato Mateus <renatomateusx@gmail.com>
2021-02-02 19:11:54 -0300

    Initial Commit

---
+++ Chris Travels/Chris_TravelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chris_TravelsApp.swift
+//  Chris Travels
+//
+//  Created by Renato Mateus on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Chris_TravelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
